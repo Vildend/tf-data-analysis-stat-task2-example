62d8ba75836bde46327230bbeebda71a86a4e454
Aynur Gatiyatullin <vildend@mail.ru>
2023-04-06 18:39:48 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -8,6 +8,5 @@ chat_id = 897901830
 
 def solution(p: float, x: np.array) -> tuple:
     alpha = 1 - p
-    x_max = numpy.max(x)
-    return ( x_max / ( 1 - alpha / 2 ) ** ( 1 / len(x) ) ), \
-           ( x_max / ( alpha / 2 ) ** ( 1 / len(x) ) )
+    x_max = np.max(x)
+    return x_max, ( ( x_max - 0.038 ) / ( alpha ) ** ( 1 / len(x) ) ) + 0.038
